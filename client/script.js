fbe99eceb510b06a4d76b0f6e9c1d7762d398cf1
sagiweizmann <sagiweizmann@gmail.com>
2024-05-19 15:45:51 +0300

    Design Changes

--- client/script.js
+++ client/script.js
@@ -29,6 +29,11 @@ function makeMove(e) {
 
 socket.on("move.made", function(data) {
     $(".board button[data-position='" + data.position + "']").text(data.symbol);
+    if(data.symbol == 'X'){
+        $(".board button[data-position='" + data.position + "']").css("color", "#0abab1");
+    } else {
+        $(".board button[data-position='" + data.position + "']").css("color", "#DAA821");
+    }
     myTurn = data.symbol !== symbol;
     renderTurnMessage();
 });
@@ -36,21 +41,26 @@ socket.on("move.made", function(data) {
 socket.on("game.begin", function(data) {
     symbol = data.symbol;
     myTurn = symbol === "X";
+    $("#player").text("You are:" + symbol);
     renderTurnMessage();
 });
 
 socket.on("game.over", function(data) {
     if (data.result === "win") {
         $("#message").text("You won!");
+        $("#message").css("color", "green");
     } else if (data.result === "lose") {
         $("#message").text("You lost.");
+        $("#message").css("color", "red");
     } else {
         $("#message").text("It's a draw.");
+        $("#message").css("color", "yellow");
     }
     $(".board button").attr("disabled", true);
 });
 
 socket.on("opponent.left", function() {
+    $("#message").css("color", "white");
     $("#message").text("Your opponent left the game.");
     $(".board button").attr("disabled", true);
 });
